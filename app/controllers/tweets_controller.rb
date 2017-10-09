03cfa8da7605b0aad040a69c441378a5d42dbc39
Geoffrey Litt <glitt@panoramaed.com>
2017-10-08 21:26:26 -0400

    fix time zones

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,8 +3,11 @@ class TweetsController < ApplicationController
     if params[:live] == "true"
       tweets = Tweet.all
     else
-      tweets =
-        Tweet.where("created_at < ?", Date.today.in_time_zone("US/Eastern"))
+      tweets = Tweet.where(
+        # limit to only tweets from the previous day, in Eastern time
+        "created_at < ?",
+        DateTime.now.in_time_zone("US/Eastern").beginning_of_day
+      )
     end
 
     @tweets = tweets.order(created_at: :desc).limit(100)

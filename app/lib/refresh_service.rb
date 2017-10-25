53151b6068376616622a8117e13fc84429f172a0
Geoffrey Litt <glitt@panoramaed.com>
2017-10-24 22:35:47 -0400

    fix token issue, need to really setup write side user

--- app/lib/refresh_service.rb
+++ app/lib/refresh_service.rb
@@ -53,8 +53,8 @@ class RefreshService
     Twitter::REST::Client.new do |config|
       config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
       config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
-      config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
-      config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+      config.access_token        = User.first.token
+      config.access_token_secret = User.first.secret
     end
   end
 end
